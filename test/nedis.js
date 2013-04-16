6739bfb574f485e52caaafaf82bc5c99fdd46c94
geisbruch <gabrieleisbruch@gmail.com>
2013-04-16 01:17:43 -0300

    Add reconnect timeouts

--- test/nedis.js
+++ test/nedis.js
@@ -5,7 +5,7 @@ var should = require("should"),
 describe("nedis", function() {
   it("Should run any redis command", function(done) {
     var client = nedis.createClient();
-    client.on("ready", function() {
+    client.on("connect", function() {
       client.do("set", "foo", "bar", function(err, data) {
         client.do("get", "foo", function(err, data) {
           data.should.equal("bar");
@@ -27,7 +27,7 @@ describe("nedis", function() {
       opError.should.be.ok;
       done();
     });
-    client.on("ready", function() {
+    client.on("connect", function() {
       client.socket.end();
       client.do("set", "foo", "bar", function(err, data) {
         should.exist(err);
@@ -35,8 +35,36 @@ describe("nedis", function() {
       });
     });
   });
-  it("Should reconnect after a specified time");
-  it("Should reconnect using a timeout function");
+  it("Should reconnect after a specified time", function(done) {
+    var client = nedis.createClient(6379, "localhost", {reconnectInterval: 50}); 
+    client.once("connect", function() {
+      var startTime = process.hrtime();
+      client.socket.end();
+      client.once("connect", function() {
+        client.do("get", "foo", function(err, data) {
+          var diff = process.hrtime(startTime);
+          (diff[1] / 1000000).should.be.above(49);
+          data.should.equal("bar");
+          done();
+        });
+      });
+    });
+  });
+  it("Should reconnect using a timeout function", function(done) {
+    var client = nedis.createClient(6379, "localhost", {reconnectInterval: function() { return 50; }}); 
+    client.once("connect", function() {
+      var startTime = process.hrtime();
+      client.socket.end();
+      client.once("connect", function() {
+        client.do("get", "foo", function(err, data) {
+          var diff = process.hrtime(startTime);
+          (diff[1] / 1000000).should.be.above(49);
+          data.should.equal("bar");
+          done();
+        });
+      });
+    });
+  });
   it("Should notify when it is connected");
   it("Should notify when it gets disconnected");
   it("Should emit timeout when an operation exceeds a specific threshold");
